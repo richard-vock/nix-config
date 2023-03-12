78fc8a72fb27a88f5349845ebc112a1d24944e32
Richard Vock <nrtn@posteo.net>
2023-03-12 10:41:38 +0100

    lazygit

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -2,59 +2,45 @@
 # Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
 
 { inputs, outputs, lib, config, pkgs, ... }: {
-  # You can import other home-manager modules here
-  imports = [
-    # If you want to use modules your own flake exports (from modules/home-manager):
-    # outputs.homeManagerModules.example
-
-    # Or modules exported from other flakes (such as nix-colors):
-    inputs.nix-colors.homeManagerModules.default
-
-    # You can also split up your configuration and import pieces of it here:
-    # ./nvim.nix
-  ];
-
-  nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # Add overlays your own flake exports (from overlays and pkgs dir):
-      outputs.overlays.additions
-      outputs.overlays.modifications
-      outputs.overlays.unstable-packages
-
-      # You can also add overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
+    imports = [
+        # If you want to use modules your own flake exports (from modules/home-manager):
+        # outputs.homeManagerModules.example
+  
+        inputs.nix-colors.homeManagerModules.default
+  
+        # You can also split up your configuration and import pieces of it here:
+        # ./nvim.nix
     ];
-    config = {
-      allowUnfree = true;
-      allowUnfreePredicate = (_: true);
+  
+    nixpkgs = {
+        # You can add overlays here
+        overlays = [
+            outputs.overlays.additions
+            outputs.overlays.modifications
+            outputs.overlays.unstable-packages
+        ];
+        config = {
+            allowUnfree = true;
+            allowUnfreePredicate = (_: true);
+        };
     };
-  };
+  
+    home = {
+        username = "richard";
+        homeDirectory = "/home/richard";
+    };
+  
+    # programs.neovim.enable = true;
+    home.packages = with pkgs; [
+        lazygit
+    ];
+  
+    programs.home-manager.enable = true;
+    programs.git.enable = true;
+  
+    colorScheme = inputs.nix-colors.colorSchemes.ayu-dark;
+  
+    systemd.user.startServices = "sd-switch";
 
-  home = {
-    username = "richard";
-    homeDirectory = "/home/richard";
-  };
-
-  # programs.neovim.enable = true;
-  # home.packages = with pkgs; [ steam ];
-
-  # Enable home-manager and git
-  programs.home-manager.enable = true;
-  programs.git.enable = true;
-
-  colorScheme = inputs.nix-colors.colorSchemes.ayu-dark;
-
-  # Nicely reload system units when changing configs
-  systemd.user.startServices = "sd-switch";
-
-  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "22.11";
+    home.stateVersion = "22.11";
 }
