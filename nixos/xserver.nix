15ab37a2b5c28ee5e7336cc29f19909c5d04722b
Richard Vock <nrtn@posteo.net>
2023-03-20 18:36:42 +0100

    first xserver working

--- nixos/xserver.nix
+++ nixos/xserver.nix
@@ -1,17 +1,19 @@
-{
-    #services.xserver = {
-    #    enable = true;
+{ config, ...}: {
+    hardware.opengl.enable = true;
+    hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
+    hardware.nvidia.modesetting.enable = true;
+    services.xserver = {
+        enable = true;
 
-    #    # hardware
-    #    videoDrivers = [ "nvidia" ];
+        # hardware
+        videoDrivers = [ "nvidia" ];
 
-    #    # wm
-    #    windowManager.i3.enable = true;
+        # wm
+        windowManager.i3.enable = true;
 
-    #    # input
-    #    layout = "us";
-    #    xkbOptions = "eurosign:e,caps:escape";
-    #    xkbVariant = "intl";
-    #};
-
+        # input
+        layout = "us";
+        xkbOptions = "eurosign:e,caps:escape";
+        xkbVariant = "intl";
+    };
 }
