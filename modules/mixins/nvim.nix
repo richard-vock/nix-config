c84f8e90a1a5ee2cd20583349e8b69b6aa5e7116
Richard Vock <nrtn@posteo.net>
2024-01-29 17:12:58 +0100

    vim easymotion and no wordmotion

--- modules/mixins/nvim.nix
+++ modules/mixins/nvim.nix
@@ -7,17 +7,16 @@
       plugins = with pkgs.vimPlugins; [
         copilot-vim
         indentLine
-        lightspeed-nvim
         nvim-tree-lua
         nvim-web-devicons
         tcomment_vim
         vim-airline
         vim-airline-themes
+        vim-easymotion
         vim-markdown
         vim-nix
         vim-polyglot
         vim-svelte
-        vim-wordmotion
         #vim-oscyank
       ];
       extraPackages = with pkgs;
@@ -108,6 +107,28 @@
         nnoremap <C-n> :NvimTreeToggle<CR>
         nnoremap <leader>r :NvimTreeRefresh<CR>
         nnoremap <leader>n :NvimTreeFindFile<CR>
+        
+        " easymotion
+        let g:EasyMotion_do_mapping = 0
+        map <Leader>j <Plug>(easymotion-j)
+        map <Leader>k <Plug>(easymotion-k)
+        nmap s <Plug>(easymotion-overwin-f)
+        nmap f <Plug>(easymotion-overwin-f2)
+        nnoremap <silent> zj :call NextClosedFold('j')<cr>
+        nnoremap <silent> zk :call NextClosedFold('k')<cr>
+        function! NextClosedFold(dir)
+            let cmd = 'norm!z' . a:dir
+            let view = winsaveview()
+            let [l0, l, open] = [0, view.lnum, 1]
+            while l != l0 && open
+                exe cmd
+                let [l0, l] = [l, line('.')]
+                let open = foldclosed(l) < 0
+            endwhile
+            if open
+                call winrestview(view)
+            endif
+        endfunction
 
         lua << EOF
           require'nvim-tree'.setup {
