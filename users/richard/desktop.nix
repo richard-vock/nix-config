f5017f0cd60c671e3b6c7f87dffc4a9915298044
Richard Vock <nrtn@posteo.net>
2025-11-06 12:43:55 +0100

    [home] remove kitty

--- users/richard/desktop.nix
+++ users/richard/desktop.nix
@@ -1,11 +1,16 @@
-{ config, lib, pkgs, inputs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
 {
   imports = [
     inputs.stylix.homeModules.stylix
     ./modules/i3.nix
     ./modules/stylix.nix
     ./modules/alacritty.nix
-    ./modules/kitty.nix
   ];
 
   xdg.enable = true;
